07ca38a3d374ae005a1b7c0b63897393d2d56736
Kiersm <kieran@ascent-creative.co.uk>
2021-03-25 17:29:30 +0000

    Attribute Updates

--- src/Models/OrderBase.php
+++ src/Models/OrderBase.php
@@ -16,13 +16,16 @@ class OrderBase extends Base
 
 
     public function items() {
-        return $this->hasMany(OrderItem::class, 'order_id');
+        return $this->hasMany(OrderItem::class, 'order_id')->with('sellable');
     }
 
     public function customer() {
         return $this->morphTo();
     }
 
+    public function getTotalQuantityAttribute() {
+        return $this->items()->sum('qty');
+    }
 
     public function getTotalAttribute() {
         $total = 0;
@@ -30,6 +33,35 @@ class OrderBase extends Base
             $total += $item->purchasePrice * $item->qty;
         }
         return $total;
+    }
+
+
+    public function hasPhysicalItems() {
+        
+        foreach($this->items()->with('sellable')->get() as $item) {
+
+            if ($item->sellable->isPhysical()) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public function hasDownloadItems() {
+
+        foreach($this->items()->with('sellable')->get() as $item) {
+
+            if ($item->sellable->isDownload()) {
+                return true;
+            }
+
+        }
+
+        return false;
+
     }
 
     
