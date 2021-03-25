aff9f1d606a5b3373665b024218dea2516a50b2a
Kiersm <kieran@ascent-creative.co.uk>
2021-03-25 17:27:22 +0000

    Order View Page

---
+++ src/resources/views/order/details.blade.php
@@ -0,0 +1,45 @@
+
+
+<table class="basket-table">
+    <thead>
+        <tr>
+            <th>Item</th>
+            <th>@if ($order->hasPhysicalItems()) Qty @endif</th>
+            <th class="text-right">Price</th>
+            <th></th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+        @foreach($order->items()->get() as $item)
+        <tr class='basket-item'>
+            <td width="100%"> <A href="{{ $item->sellable->url }}">{{ $item->sellable->getItemName() }}</A> </td>
+            
+            <td> 
+                @if ($item->sellable->isPhysical())
+                    {{ $item->qty }} 
+                @endif
+            </td>
+            <td class="text-right">&pound;{{ number_format($item->purchasePrice, 2) }}</td>
+            <td>
+                @if ($item->sellable->isDownload())
+                    <A href="{{ $item->getDownloadUrl() }}" class="bi-cloud-arrow-down-fill" style="font-size: 2rem; line-height: 1rem;" data-toggle="tooltip" title="Download File"></A>
+                @endif
+            </td>
+        </tr>
+        @endforeach
+
+    </tbody>
+
+    <tfoot>
+        <tr>
+            <th></th>
+            <th class="text-right" >Total:</th>
+            <th class="text-right">&pound;{{ $order->total }}</th>
+            <th></th>
+        </tr>
+    </tfoot>
+
+    
+</table>    
