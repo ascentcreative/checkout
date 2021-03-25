519626944ab7e516f816f7fa83b4ddce036244a3
Kiersm <kieran@ascent-creative.co.uk>
2021-03-25 17:30:35 +0000

    Order Viewing

---
+++ src/Controllers/OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AscentCreative\Checkout\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Storage;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+use AscentCreative\Checkout\Models\Basket;
+use AscentCreative\Checkout\Models\Order;
+
+
+
+class OrderController extends Controller
+{
+   
+    public function show(Order $order) {
+
+        if($order->customer == Auth::user()) {
+
+            $pageTitle = "Order " . $order->orderNumber;
+            headTitle()->add($pageTitle);
+    
+            return view('checkout::order.show')->with('order', $order)->with('pageTitle', $pageTitle);
+            
+
+        } else {
+
+           abort(404);
+        }
+    }
+
+
+
+
+
+
+
+}
