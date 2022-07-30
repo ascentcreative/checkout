001486bf3a1bc1e10246f60f965ab05ab382c609
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-30 23:09:51 +0100

    Offer code field

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -1,18 +1,37 @@
 <div>
     <H1 style="font-size: 2rem;">Your Basket</H1>
 
-    <div class="grid" style="display: grid; gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr))">
 
+    
+
+    <div class="grid" style="display: grid; gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr))">
 
         <div style="grid-column: 1 / span 2;">
     
             <div class="border bg-white p-5">
 
+               
                 @include('checkout::basket.livewire.contents')
                 
                 @if(!basket()->isEmpty)
-                <div class="text-right mt-3">
-                    <button wire:click="clear" class="button btn btn-secondary btn-sm">Clear Basket</button>
+
+                <div class="flex flex-between mt-3">
+
+                    <form wire:submit.prevent="setCode(Object.fromEntries(new FormData($event.target)))">
+                        <div class="input-group">
+                        {{-- <input type="text" placeholder="Add Code..." /> --}}
+                        <x-forms-fields-input type="text" name="code" value="" label="code" wrapper="none" placeholder="Enter Code" />
+                        <div class="input-group-append">
+                            <button class="btn btn-primary btn-sm">Apply</button>
+                        </div>
+                        </div>
+                    </form>
+                    
+        
+                    <div class="text-right">
+                        <button wire:click="clear" class="button btn btn-secondary btn-sm">Clear Basket</button>
+                    </div>
+
                 </div>
                 @endif
                 {{-- @yield('basket.contentstable') --}}
