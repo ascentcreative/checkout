e76c84640325e751294dd322cea29b7d449f1ac0
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-11-02 11:53:02 +0000

    Basket manager class

---
+++ src/BasketManager.php
@@ -0,0 +1,59 @@
+<?php
+namespace AscentCreative\Checkout;
+
+use AscentCreative\Checkout\Models\Basket;
+
+class BasketManager {
+
+    /** Singleton Setup */
+    private static $_instance = null;
+
+    static function getInstance() {
+        if (self::$_instance == null) {
+            self::$_instance = new BasketManager();
+        }
+ 
+        return self::$_instance;
+    }
+
+
+    /** Instance elements */
+    public $basket = null;
+
+    public $items = null;
+
+    public $customer = null;
+
+    public $address = null;
+
+    public $shipping_service = null;
+
+
+
+    public function __construct() {
+        
+        if(!session()->has('checkout_basket_id')) {
+            $this->basket = new Basket();
+        } else {
+            $this->basket = Basket::where('id', session('checkout_basket_id'))->with('items')->with('items.sellable')->with('customer')->first();
+            if($this->basket == null) {
+                $this->basket = new Basket();
+            }
+        }
+
+    }
+
+    public function getItems() {
+
+        return $this->basket->items;
+
+    }
+
+    public function hasPhysicalItems() {
+        return $this->basket->hasPhysicalItems();
+    }
+
+
+
+}
+
